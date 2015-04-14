af48da7291909c7a69f458d5caca027e57577997
Sam Hopkins <safhopkins@yahoo.com>
2015-04-14 13:50:37 -0500

    pushing to heroku

--- MarkovLyrics/settings.py
+++ MarkovLyrics/settings.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 TEMPLATE_DIRS = [os.path.join(BASE_DIR, 'templates')]
@@ -57,13 +56,20 @@ WSGI_APPLICATION = 'MarkovLyrics.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql_psycopg2',
+        "NAME": "random",
+        "USER": "sam",
+        "PASSWORD": '',
+        "HOST": 'localhost',
+        "PORT": '',
     }
 }
+
+import dj_database_url
+
+DATABASES[‘default’] = dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
@@ -78,6 +84,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Allow all host headers
+ALLOWED_HOSTS = ['serene-dawn-4963.herokuapp.com']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
@@ -87,4 +95,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+
 
